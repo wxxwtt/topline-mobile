ce382115134461afc66784d8617be5843775fb64
weixinxiu <weixinxiu@digilinx.cn>
2021-02-20 17:59:57 +0800

    修改接口地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,8 @@ import jsonBig from 'json-bigint'
 import store from '@/store'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
+  // baseURL: 'http://toutiao-app.itheima.net'
+  // baseURL: 'http://api-toutiao-web.itheima.net'
 })
 
 request.defaults.transformResponse = [function (data) {
